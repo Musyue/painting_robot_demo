4532f10ec40f947d3b335c344e0a2cba23348cd5
Musyue <cboy@mail.ustc.edu.cn>
2020-03-25 17:50:14 +0800

    1,update coverage_path_planning

--- scripts/coverage_path_planning/coverage_planning_offline_without_sensor.py
+++ scripts/coverage_path_planning/coverage_planning_offline_without_sensor.py
@@ -16,44 +16,37 @@ class Renovationrobot_inverse_kinematics():
     def __init__(self):
         self.parameterx=0.430725381079
         self.parametery=-0.00033063639818
-        self.parameterz=0.1
+        self.parameterz=0.028625
         self.interval=0.10
-        self.mobile_base_point=[]
-    def print_json(self,data):
-        print(json.dumps(data, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False))
+
     def renovationrobot_joints_computation(self,manipulatorbase_targetpose_onecell,manipulatorendeffector_targetpose_onecell):
+
         # computation of target joints of mobile platform
         theta_z=manipulatorbase_targetpose_onecell[0][5]
         deltax=self.parameterx*cos(theta_z)-self.parametery*sin(theta_z)
         deltay=self.parameterx*sin(theta_z)+self.parametery*cos(theta_z)
         mobileplatform_targetjoints=[manipulatorbase_targetpose_onecell[0][0]-deltax,-(manipulatorbase_targetpose_onecell[0][1]-deltay),theta_z]
-        
-        self.mobile_base_point.append(mobileplatform_targetjoints)
+
+        # computation of mobile platform collision-avoidance path
+        # input: the present position of mobile platform
+        # input: the next position of mobile platform
+        # output: collision-avoidance path between the present position and next position
+
+
         # computation of target joints of rodclimbing_robot
         rodclimbing_robot_targetjoints=[manipulatorbase_targetpose_onecell[0][2]-self.parameterz,0.0]
-
-        # computation of forward paths of manipulator
-        manipulatorendeffector_targetpose_onecell_new=np.zeros(manipulatorendeffector_targetpose_onecell.shape)
-        for i in range(len(manipulatorendeffector_targetpose_onecell_new)/2):
-            if i%2==0:
-                for j in range(len(manipulatorendeffector_targetpose_onecell_new[i])):
-                    manipulatorendeffector_targetpose_onecell_new[2*i][j]=manipulatorendeffector_targetpose_onecell[2*i][j]
-                    manipulatorendeffector_targetpose_onecell_new[2*i+1][j]=manipulatorendeffector_targetpose_onecell[2*i+1][j]
-
-            else:
-                for j in range(len(manipulatorendeffector_targetpose_onecell_new[i])):
-                    manipulatorendeffector_targetpose_onecell_new[2*i][j]=manipulatorendeffector_targetpose_onecell[2*i+1][j]
-                    manipulatorendeffector_targetpose_onecell_new[2*i+1][j]=manipulatorendeffector_targetpose_onecell[2*i][j]
+        # motion of rod climbing robot
+        # print("rodclimbing_robot_targetjoints=",rodclimbing_robot_targetjoints)
 
         # computation of inverse joints of manipulator
-        aubo_joints_list=np.array([0.7432146906113353, -0.6072259915236845, 1.387201205355398, 1.9944271968790823, 0.8275816361835613, 1.5707963267948966])
+        aubo_joints_list=np.array([-0.2852509833270265, -0.5320376301933496, 1.3666906155038931, -1.2428644078925508, -1.856047310121923,1.5707963267948966])
         previous_aubo_joints=aubo_joints_list
-        for i in range(len(manipulatorendeffector_targetpose_onecell_new)-1):
-            p1=np.array([manipulatorendeffector_targetpose_onecell_new[i][0],manipulatorendeffector_targetpose_onecell_new[i][1],manipulatorendeffector_targetpose_onecell_new[i][2]])
-            p2=np.array([manipulatorendeffector_targetpose_onecell_new[i+1][0],manipulatorendeffector_targetpose_onecell_new[i+1][1],manipulatorendeffector_targetpose_onecell_new[i+1][2]])
+        for i in range(len(manipulatorendeffector_targetpose_onecell)-1):
+            p1=np.array([manipulatorendeffector_targetpose_onecell[i][0],manipulatorendeffector_targetpose_onecell[i][1],manipulatorendeffector_targetpose_onecell[i][2]])
+            p2=np.array([manipulatorendeffector_targetpose_onecell[i+1][0],manipulatorendeffector_targetpose_onecell[i+1][1],manipulatorendeffector_targetpose_onecell[i+1][2]])
             distance=sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2+(p1[2]-p2[2])**2)
             p=np.zeros(3)
-            if i==len(manipulatorendeffector_targetpose_onecell_new)-2:
+            if i==len(manipulatorendeffector_targetpose_onecell)-2:
                 num = int(distance / self.interval)+2
             else:
                 num = int(distance / self.interval)+1
@@ -61,21 +54,32 @@ class Renovationrobot_inverse_kinematics():
                 p[0] = p1[0] + (p2[0] - p1[0]) / distance * self.interval * j
                 p[1] = p1[1] + (p2[1] - p1[1]) / distance * self.interval * j
                 p[2] = p1[2] + (p2[2] - p1[2]) / distance * self.interval * j
-                q=np.array([manipulatorendeffector_targetpose_onecell_new[i][3],manipulatorendeffector_targetpose_onecell_new[i][4],manipulatorendeffector_targetpose_onecell_new[i][5]])
+                q=np.array([manipulatorendeffector_targetpose_onecell[i][3],manipulatorendeffector_targetpose_onecell[i][4],manipulatorendeffector_targetpose_onecell[i][5]])
                 T_mat_generation=pose2mat()
                 mat=T_mat_generation.mat4x4(p,q)
-                # print("the robotic matrix is:",mat)
                 mat1=np.ravel(mat)
                 mat2=mat1.tolist()
                 aubo_arm = Aubo_kinematics()
+                q_reslut_dic,num_sols=aubo_arm.aubo_inverse(mat2)
+                # print("q result is:")
+                # for i in range(len(q_reslut_dic)):
+                #     print(q_reslut_dic[i])
                 aubo_joints_onepoint=aubo_arm.GetInverseResult(mat2,previous_aubo_joints)
+                # print("selected aubo joints is",aubo_joints_onepoint)
                 previous_aubo_joints=aubo_joints_onepoint
-                # print("aubo_joints_onepoint=:",aubo_joints_onepoint)
                 aubo_joints_list=np.append(aubo_joints_list,aubo_joints_onepoint,axis=0)
-        aubo_targetjoints=aubo_joints_list.reshape(len(aubo_joints_list)/6,6)
 
-        return mobileplatform_targetjoints, rodclimbing_robot_targetjoints,aubo_targetjoints
+        points_num=len(aubo_joints_list)/6
+        for i in range(points_num):
+            # motion of manipulator
+            aubo_joints=np.array(aubo_joints_list[6*i:6*i+6])
+            # print('aubo_joints=:',aubo_joints)
 
+        aubo_targetjoints = aubo_joints_list.reshape(len(aubo_joints_list) / 6, 6)
+        return mobileplatform_targetjoints, rodclimbing_robot_targetjoints, aubo_targetjoints
+
+    def print_json(self,data):
+        print(json.dumps(data, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False))
     def array_to_dictlist(self,data):
         datadict={}
         for i in range(len(data)):
@@ -83,7 +87,7 @@ class Renovationrobot_inverse_kinematics():
             datadict.update({("aubo_data_num_"+str(i)):list(data[i])})
         return datadict
 if __name__ == "__main__":
-    mat_path="/data/ros/yue_ws_2020/src/painting_robot_demo/data/data.mat"
+    mat_path="/data/ros/yue_wk_2019/src/painting_robot_demo/data/data.mat"
     data = io.loadmat(mat_path)
     # print(data) 
     manipulatorbase_targetpose=data['renovation_cells_manipulatorbase_positions']
